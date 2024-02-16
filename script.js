4bc58baa8810448aaa92de61b84a32e9e350261f
bhaswat <xbhaswat@gmail.com>
2024-02-16 19:47:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav", {
+        y: `-10`,
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        stagger: .2
+        
+    })
+}
+
+
+function minicircleSkew(){
+    window.addEventListener("mousemove", function(dets){
+        
+    });
+}
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets) {
+      document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
